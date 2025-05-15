d24adb635ddfa03743f499e1c7ee27a031d3bb74
Aravinth <varavinth2001@gmail.com>
2025-05-15 23:41:28 +0530

    Added the STGP event photos

---
+++ public/javascript/stgp-event.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slideshows = document.querySelectorAll('.flat-row .row .right-slideshow-box');
+
+    slideshows.forEach((slideshow) => {
+      const slides = slideshow.querySelectorAll('.flat-row .row .right-slideshow-box .slide');
+      let index = 0;
+
+      if (slides.length <= 1) return; // Skip if only 1 slide
+
+      setInterval(() => {
+        slides[index].classList.remove('active');
+        index = (index + 1) % slides.length;
+        slides[index].classList.add('active');
+      }, 3000); // Change slide every 3 seconds
+    });
+  });
